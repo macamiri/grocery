df09efb99758745b6624d7eec03347cc171d7240
moamiristat <moamiristat@protonmail.com>
2022-02-16 18:07:13 -0500

    Collect store info - for 1 location

--- data/collect_data.R
+++ data/collect_data.R
@@ -23,5 +23,45 @@ initiate_server <- rsDriver(port = netstat::free_port(),
 remDr <- initiate_server$client
 remDr$open()
 
+##### Selenium to collect data -----
+### How many links to click in the '1st-layer'?
+remDr$navigate(url)
+cat(crayon::blue(remDr$getTitle()))
+
+# Find all available locations
+rem_locations <- remDr$findElements(using = "class name", 
+                                    value = "text-success")
+locations <- rem_locations %>% 
+  map(~ .$getElementText()) %>% 
+  unlist()
+locations
+# There are 131 links to click in the '1st-layer'
+
+### Collect the store info for each location
+# Click on location link
+rem_locations_link <- remDr$findElement(using = "link text", 
+                                        value = "Abu Hail")
+
+rem_locations_link$clickElement()
+
+# Get store info: (1) click on each one, (2) collect data
+# (1)
+rem_store_info <- remDr$findElements(using = "class name", 
+                                     value = "store-info")
+
+rem_store_info %>% 
+  map(~ .clickElement()) %>% 
+  unlist()
+
+# (2)
+rem_store_details <- remDr$findElements(using = "class name", 
+                                        value = "store-detail")
+store_details <- rem_store_details %>% 
+  map(~ .$getElementText()) %>% 
+  unlist() %>% 
+  str_replace_all("\n", " ")
+
+### Collect category data from each store
+#...
 
 
