07a0503d26dc8f2f34b45d67b47842752be1ebaf
moamiristat <moamiristat@protonmail.com>
2022-02-16 16:01:07 -0500

    Check which webpages are not bot friendly

--- data/collect_data.R
+++ data/collect_data.R
@@ -1,1 +1,15 @@
-# Collect data
+### Load packages -----
+pacman::p_load(robotstxt, RSelenium, rvest)
+
+### Confirm which webpages are not bot friendly -----
+url <- "https://www.elgrocer.com"
+
+rtxt <- robotstxt(domain = url)
+rtxt$comments
+rtxt$crawl_delay
+rtxt$permissions
+
+paths_allowed(domain = url, paths = c("/store", "/stores"))
+
+# We can collect data from the webpages we are interested in
+
