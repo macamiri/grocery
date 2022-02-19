e1174d0ae8ff68926e33a52f7275bf684c6ef058
moamiristat <moamiristat@protonmail.com>
2022-02-18 22:20:03 -0500

    Load packages & sample data

--- data/clean_data.R
+++ data/clean_data.R
@@ -1,1 +1,18 @@
-# Clean collected sample data: store ---> category ---> subcategory ---> item
+# Clean collected sample data: location ---> store ---> (sub)category ---> item
+
+##### 1: Load packages -----
+# Main packages loaded: purrr, dplyr, readr, stringr
+# Packages used with namespace:
+pacman::p_load(purrr, dplyr, readr, stringr)
+
+##### 2: Load in sample data -----
+data_files <- fs::dir_ls(here::here("data"), regexp = ".csv$")
+
+data_tibble <- 
+  data_files %>% 
+  map(., ~ read_csv(.x, col_types = cols(.default = col_character()))) %>% 
+  set_names(data_files %>% str_extract("[^/]*$") %>% str_remove(".csv"))
+
+nested_grocery <- tibble::enframe(data_tibble, name = "layer", value = "data")
+
+
