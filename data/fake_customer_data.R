70b673864577cc9b3011ae2ff6dbc7c6acd7dd59
moamiristat <moamiristat@protonmail.com>
2022-03-04 23:58:23 -0500

    Create smaller data set (for 1 store)

--- data/fake_customer_data.R
+++ data/fake_customer_data.R
@@ -9,7 +9,7 @@ unnest_origin <- function(main_table = nested_grocery, filter_table) {
     purrr::pluck(1)
 }
 
-select_products <- function(id = order_db$customer_id, num_of_products_mean = 16, sd = 4) {
+select_products <- function(id = order_db$customer_id, num_of_products_mean = 26, sd = 4) {
   id %>% 
     map(., function(.x) {
       num_of_products <- round(rnorm(1, mean = num_of_products_mean, sd = sd))
@@ -30,6 +30,29 @@ select_products <- function(id = order_db$customer_id, num_of_products_mean = 16
     map(., ~ paste0(.x, collapse = "@")) %>% 
     unlist()
 }
+
+select_products_funmart <- function(id = order_funmart_db$customer_id, num_of_products_mean = 9, sd = 2) {
+  id %>% 
+    map(., function(.x) {
+      num_of_products <- round(rnorm(1, mean = num_of_products_mean, sd = sd))
+      
+      if(num_of_products >= 3) {
+        sample(product_prob_funmart$product, 
+               size = num_of_products, 
+               prob = product_prob_funmart$probs, 
+               replace = FALSE)
+      } else {
+        num_of_products <- 3
+        sample(product_prob_funmart$product, 
+               size = num_of_products, 
+               prob = product_prob_funmart$probs, 
+               replace = FALSE)
+      }
+    }) %>% 
+    map(., ~ paste0(.x, collapse = "@")) %>% 
+    unlist()
+}
+
 
 
 ##### 3: Load grocery data *_for_analysis -----
@@ -73,7 +96,7 @@ num_of_products <-
   nrow()
 
 score_column <- 
-  sample(30:90, size = num_of_products, replace = TRUE) %>% 
+  sample(1:39, size = num_of_products, replace = TRUE) %>% 
   tibble::tibble(score = .)
 
 grocer_products <- 
@@ -86,12 +109,17 @@ grocer_products <-
 product_prob <- 
   ocado %>% 
     dplyr::group_by(product) %>% 
-    dplyr::summarise(score = (as.numeric(num_of_reviews) + as.numeric(recommend)) * .25) %>% 
+    dplyr::summarise(score = ceiling((as.numeric(num_of_reviews) + as.numeric(recommend)) * .5)) %>% 
     dplyr::filter(!is.na(score)) %>% 
     dplyr::ungroup() %>% 
     dplyr::bind_rows(grocer_products) %>% 
     dplyr::mutate(probs = score / sum(score)) %>% 
     dplyr::arrange(desc(score))
+
+product_prob_funmart <- 
+  tibble::tibble(
+    product = sample(product_prob$product, size = 200, replace = FALSE)) %>% 
+    dplyr::mutate(probs = probs(j = 200))
 
 # Prices for all 12,539 products
 prices <- 
@@ -107,7 +135,7 @@ prices <-
                        dplyr::select(item, price) %>% 
                        dplyr::rename("product" = item))
 
-##### 5: Create fake random customer data -----
+##### 5A: Create fake random customer data -----
 num_of_customers <- 100000
 num_of_orders <- 250000
 
@@ -168,3 +196,64 @@ basket_db <- fabricate(
   dplyr::distinct(basket_id, product, .keep_all = TRUE)
 
 # readr::write_csv(basket_db, here::here("data/basket_db.csv"))
+
+
+##### 5B: Create fake random customer data for 1 store (funmart) -----
+num_of_customers <- 1000
+num_of_orders <- 2500
+
+# Customer database table - keep distinct names
+customer_funmart_db <- fabricate(
+  N = num_of_customers, 
+  customer_name = randomNames(n = N, name.sep = " ", name.order = "first.last"), 
+  long = ch_position(n = N, bbox = c(51, 22.50, 56.25, 26)) %>% purrr::map_chr(., ~ .[1]), 
+  lat = ch_position(n = N, bbox = c(51, 22.50, 56.25, 26)) %>% purrr::map_chr(., ~ .[2]),
+) %>% 
+  tibble::as_tibble() %>% 
+  dplyr::distinct(customer_name, .keep_all = TRUE) %>% 
+  dplyr::rename("customer_id" = ID)
+
+# readr::write_csv(customer_funmart_db, here::here("data/customer_funmart_db.csv"))
+
+# Orders database table
+order_funmart_db <- fabricate(
+  N = num_of_orders, 
+  customer_id = as.character(sample(customer_funmart_db$customer_id, size = N, replace = TRUE)), 
+  order_date = date_stamp(n = N, 
+                          start = as.Date("2020-01-01"), 
+                          k = 731, 
+                          by = "1 day", 
+                          prob = c(rep(0.0006557377, 183), 
+                                   rep(0.001530055, 183), 
+                                   rep(0.0009836066, 183), 
+                                   rep(0.002307692, 182))), 
+  order_time = hour(n = N, 
+                    x = seq(from = 0, to = 23.9, by = .1), 
+                    prob = c(rep(0.000625, 80), 
+                             rep(0.01, 20), 
+                             rep(0.0125, 20), 
+                             rep(0.004166667, 60), 
+                             rep(0.00375, 40), 
+                             rep(0.005, 20))), 
+  store = rep("Funmart", N),
+) %>% 
+  tibble::as_tibble() %>% 
+  dplyr::rename("order_id" = ID)
+
+# readr::write_csv(order_funmart_db, here::here("data/order_funmart_db.csv"))
+
+
+# Basket line item database table - join later if need more ocado product info
+basket_funmart_db <- fabricate(
+  N = num_of_orders, 
+  order_id = as.character(sample(order_funmart_db$order_id, size = N, replace = FALSE)), 
+  product = select_products_funmart(), 
+) %>% 
+  tibble::as_tibble() %>% 
+  tidyr::separate_rows(product, sep = "@") %>% 
+  dplyr::left_join(prices, by = "product") %>% 
+  dplyr::select(1:3, price) %>% 
+  dplyr::rename("basket_id" = ID) %>% 
+  dplyr::distinct(basket_id, product, .keep_all = TRUE)
+
+# readr::write_csv(basket_funmart_db, here::here("data/basket_funmart_db.csv"))
