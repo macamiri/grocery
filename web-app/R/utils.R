e2c35dfa1b2e1468b4d6b1246c67890bfa12fdb7
moamiristat <moamiristat@protonmail.com>
2022-06-06 21:25:17 -0400

    Remove theme

--- web-app/R/utils.R
+++ web-app/R/utils.R
@@ -1,40 +1,5 @@
 # Load packages
-library(fresh)
 library(grocerycart)
-
-##### Create theme ------------------------------------------------------------
-# mytheme <- create_theme(
-#   bs4dash_vars(
-#     navbar_light_color = "#bec5cb",
-#     navbar_light_active_color = "#FFF",
-#     navbar_light_hover_color = "#FFF"
-#   ),
-#   bs4dash_yiq(
-#     contrasted_threshold = 10,
-#     text_dark = "#FFF", 
-#     text_light = "#272c30"
-#   ),
-#   bs4dash_layout(
-#     main_bg = "#353c42"
-#   ),
-#   bs4dash_sidebar_light(
-#     bg = "#272c30", 
-#     color = "#bec5cb",
-#     hover_color = "#FFF",
-#     submenu_bg = "#272c30", 
-#     submenu_color = "#FFF", 
-#     submenu_hover_color = "#FFF"
-#   ),
-#   bs4dash_status(
-#     primary = "#5E81AC", danger = "#BF616A", light = "#272c30"
-#   ),
-#   bs4dash_color(
-#     gray_900 = "#FFF"
-#   )
-# )
-
-
-#source(here::here("analysis/basket_analysis.R"))
 
 ##### Source analysis files -----
 source(here::here("analysis/eg_analysis.R"))
@@ -116,6 +81,3 @@ products_available <-
   select(product) %>% 
   unique() %>% 
   arrange(product)
-
-
-
